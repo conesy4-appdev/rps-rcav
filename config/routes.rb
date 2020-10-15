18daabbbe91ff64b41ccb4a7e529e6e3a7a20135
conesy4 <mconan@chicagobooth.edu>
2020-10-15 04:29:14 +0000

    rps 4

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get("/paper", { :controller => "application", :action => "play_paper" })
 
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
+
 end
